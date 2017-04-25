6b53a578333c49c98349ff07e3457946786be927
TheSadFrog <909829990@qq.com>
2017-04-25 22:41:55 +0800

    添加BottomNavigation的例子

---
+++ BottomNavigation/app/src/main/java/com/zhouchatian/bottomnavigation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zhouchatian.bottomnavigation;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    private BottomNavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.text);
+        mNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mTextView.setText(item.getTitle().toString().toUpperCase());
+                return true;
+            }
+        });
+    }
+}
